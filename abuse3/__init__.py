16a27d16e0b34a210fa2283d70daa98b30dd650d
Jason G <18637887+techmad@users.noreply.github.com>
2023-09-30 19:35:56 -0500

    push

--- abuse3/__init__.py
+++ abuse3/__init__.py
@@ -1,9 +1,13 @@
 import requests
 import json
+import yaml
 
 def integration(instance, params, config = { "timeout" : 15 }):
 
     url = instance["url"] # "https://mb-api.abuse.ch/api/v1/") 
+
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
 
     try:
         response = requests.post(url, data=params, timeout=config['timeout'])
@@ -12,6 +16,7 @@ def integration(instance, params, config = { "timeout" : 15 }):
         json_response = json.loads(json_response)['data']
         return {"status": "OK",
                  "response" : json_response,
+                 "config" : config,
                  "type" : "list-objects"
         }
     
