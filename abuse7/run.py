79dc16161d2e13f9600f687949daa4b16bacee13
Jason G <18637887+techmad@users.noreply.github.com>
2023-10-01 08:41:52 -0500

    push

--- abuse7/run.py
+++ abuse7/run.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def integration(instance, params, config = { "timeout" : 27 }):
+def integration(instance, params, config = { "timeout" : 28 }):
 
     url = instance["url"] # "https://mb-api.abuse.ch/api/v1/") 
 
